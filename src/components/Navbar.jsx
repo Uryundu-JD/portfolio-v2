17ac323217d376c196e258ea2a460f4663f7bdc1
JD Oriondo <jdoriondo@investagrams.com>
2026-03-26 23:00:41 +0800

    Change cv dir

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/cv_Oriondo-JD.pdf';
+    const resumeUrl = {`${import.meta.env.BASE_URL}cv_Oriondo-JD.pdf`};
     window.open(resumeUrl);
   };
 
