70f0c98820f5bc7d90d57bb5fdb529faabaa5fcc
JD Oriondo <jdoriondo@investagrams.com>
2026-03-26 23:28:31 +0800

    change cv uri

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/Resume.pdf';
+    const resumeUrl = '/portfolio-v2/Resume.pdf';
     window.open(resumeUrl);
   };
 
